d52b3ab34361cbfa6558bbc85a1333a391e2593f
hilamica <hila.michaeli.tlv@gmail.com>
2018-04-30 23:18:49 +0300

    change herulo

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const books = require('./mock/db.json')
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.get('/booksList', function (req, res) {
+
+    res.send(books);
 });
 
 // Start the app by listening on the default Heroku port
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  api: 'https://localhost:5000'
+  api: 'https://localhost:8080'
 };
