f940931105f1c1184c50ee6e5dde8a9f814f3d00
hilamica <hila.michaeli.tlv@gmail.com>
2018-05-01 14:23:46 +0300

    adding packeges

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-//Install express server
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+// const books = require('.assets/db.json')
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist'));
+
+// app.get('/*', function (req, res) {
+
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
+// app.get('/booksList', function (req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send(books);
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
 const express = require('express');
 const path = require('path');
-const books = require('./assets/db')
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-app.get('/booksList', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(books);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
