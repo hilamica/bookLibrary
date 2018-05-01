84298e86d70b3b6a383c6dcbe8e87c53e3db7c2c
hilamica <hila.michaeli.tlv@gmail.com>
2018-05-01 14:42:01 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const books = require('./dist/assets/db');
+const books = require('./dist/assets/db.json');
 
 const app = express();
 
