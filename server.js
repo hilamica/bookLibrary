39c1a5f738032c107f2dbb5a94cf439c4e3164dd
hilamic <hilam@ui.co.il>
2018-05-02 14:07:30 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/index', function (req, res) {
@@ -17,22 +15,6 @@ app.get('/booksList', function (req, res) {
     res.send(books.booksList);
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
-
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname+'/dist/index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
